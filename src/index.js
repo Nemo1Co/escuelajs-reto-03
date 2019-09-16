d72a9a28df02d57d55b1118c6fd73ae6c3b6bc63
Jorge Humberto Nemoga Pinzon <jhnemogap@gmail.com>
2019-09-16 18:48:51 -0500

    Paso 2 terminado

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest
-const API = 'https://rickandmortyapi.com/api/character/'
 const XHttps = new XMLHttpRequest()
+
+const API = 'https://rickandmortyapi.com/api/character/'
+const RESULTS_NUMB = 0
 
 const fetchData = (url_api, callback) => {
   XHttps.onreadystatechange = (event) => {
     if (XHttps.readyState === 4) {
-      if (XHttps.status == 200) return callback(null, XHttps.responseText)
-      else return callback(url_api)
+      if (XHttps.status == 200) {
+        return callback(null, JSON.parse(XHttps.responseText))
+      }
+      else {
+        return callback(url_api)
+      }
     }
   }
   XHttps.open('GET', url_api, false)
@@ -15,24 +21,23 @@ const fetchData = (url_api, callback) => {
 
 fetchData(
   API, 
-  (error, data1) => {
-    if (error) return console.error(`Error 1: ${error}`);
-    data1 = JSON.parse(data1)
+  (error, dataAll) => {
+    if (error) return console.error(`Error 1: ${error}`)
 
     fetchData(
-      `${API}${data1.results[0].id}`, 
-      (error, data2) => {
-        if (error) return console.error(`Error 3: ${error}`)
-        data2 = JSON.parse(data2)
-        
+      `${API}${dataAll.results[RESULTS_NUMB].id}`, 
+      (error, character) => {
+        if (error) return console.error(`Error 2: ${error}`)
+
         fetchData(
-          data2.origin.url, 
-          (error3, data3) => {
-            if (error3) return console.error(`Error 3: ${error}`)
-            data3 = JSON.parse(data3)
-            console.log(`Personajes: ${data1.info.count}`)
-            console.log(`Primer Personaje: ${data2.name}`)
-            console.log(`Dimensión: ${data3.dimension}`)
+          character.origin.url, 
+          (error, characterLocation) => {
+            if (error) return console.error(`Error 3: ${error}`)
+
+            console.log(
+              `Personajes: ${dataAll.info.count}\n` + 
+              `Personaje ${character.id}: ${character.name}\n` +
+              `Dimensión: ${characterLocation.dimension}\n`)
           }
         )
       }
