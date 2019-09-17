1cfadbc2af9469681cc44a2d4f2e22be293a4b86
Jorge Humberto Nemoga Pinzon <jhnemogap@gmail.com>
2019-09-16 22:44:53 -0500

    Paso 3 Terminado

--- src/index.js
+++ src/index.js
@@ -2,7 +2,42 @@ const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest
 const XHttps = new XMLHttpRequest()
 
 const API = 'https://rickandmortyapi.com/api/character/'
-const CHARACTER_ID = 0
+const RESULTS_NUMB = 0
+
+const fetchData = (url_api) => {
+  return new Promise((resolve, reject) => {
+    XHttps.onreadystatechange = (event) => {
+      if (XHttps.readyState === 4) {
+        if (XHttps.status == 200) resolve(JSON.parse(XHttps.responseText))
+        else reject(`Error URL: ${url_api}`)
+      }
+    }
+    XHttps.open('GET', url_api, false)
+    XHttps.send()
+  })
+}
+
+fetchData(API)
+.then(r => {
+  console.info(`Personajes: ${r.info.count}`)
+  return fetchData(`${API}${r.results[RESULTS_NUMB].id}`)
+})
+.then(r => {
+  console.info(`Personaje No. ${r.id}: ${r.name}`)
+  return fetchData(r.origin.url)
+})
+.then(r => {console.info(`Dimensión: ${r.dimension}`)})
+.catch(e => {console.error(e)})
+
+
+// +++++++++++++++++++++++++++++
+// ++   Solución del paso 2   ++
+/* 
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest
+const XHttps = new XMLHttpRequest()
+
+const API = 'https://rickandmortyapi.com/api/character/'
+const RESULTS_NUMB = 0
 
 const fetchData = (url_api, callback) => {
   XHttps.onreadystatechange = (event) => {
@@ -25,7 +60,7 @@ fetchData(
     if (error) return console.error(`Error 1: ${error}`)
 
     fetchData(
-      `${API}${dataAll.results[CHARACTER_ID].id}`, 
+      `${API}${dataAll.results[RESULTS_NUMB].id}`, 
       (error, character) => {
         if (error) return console.error(`Error 2: ${error}`)
 
@@ -36,7 +71,7 @@ fetchData(
 
             console.log(
               `Personajes: ${dataAll.info.count}\n` + 
-              `Primer Personaje: ${character.name}\n` +
+              `Personaje ${character.id}: ${character.name}\n` +
               `Dimensión: ${characterLocation.dimension}\n`)
           }
         )
@@ -44,3 +79,4 @@ fetchData(
     )
   }
 )
+*/
